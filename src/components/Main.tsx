e6a60c0efe86d540003a7164c08922c7c754fe78
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2023-03-12 19:08:31 -0600

    checking if mobile and changing meta theme-color to match theme

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,32 @@
 import Nav from "./Nav";
 import CallToAction from "./CallToAction";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ThemeContext } from "./context/ThemeContext";
 
 import { Paper, ThemeProvider as MuiThemeProvider } from "@mui/material";
 import { muiDarkTheme, muiLightTheme } from "./context/muiTheme";
 import AnimatedBackground from "./AnimatedBackground";
+import MobileNav from "../components/MobileNav";
 
 const Main = () => {
   const { theme } = useContext(ThemeContext);
+  let isMobile = window.innerWidth <= 850;
+  let navigationMenu = isMobile ? <MobileNav /> : <Nav />;
+
+  useEffect(() => {
+    let metaTheme = document.querySelector('meta[name="theme-color"]');
+    if (theme === "dark" && metaTheme !== null) {
+      metaTheme.setAttribute("content", "#000");
+    } else if (theme === "light" && metaTheme !== null) {
+      metaTheme.setAttribute("content", "#FFF");
+    }
+  }, [theme]);
+
   return (
     <MuiThemeProvider theme={theme === "dark" ? muiDarkTheme : muiLightTheme}>
       <Paper>
         <AnimatedBackground />
-        <Nav />
+        {navigationMenu}
         <CallToAction />
       </Paper>
     </MuiThemeProvider>
